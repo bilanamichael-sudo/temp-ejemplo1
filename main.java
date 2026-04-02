99b79c7cc27964de51bb970ebb50365ebdf99786
michaelbilaña <bilanamichael@gmail.coma>
2026-04-02 08:04:43 -0500

    correccion de errores

--- main.java
+++ main.java
@@ -1,8 +1,16 @@
-
+import java.util.*;
 
 public class main {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+
+        String nombre;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre");
+        nombre = entrada.next();
+
+        System.out.println("El noombre es: " + nombre);
     }
 }
